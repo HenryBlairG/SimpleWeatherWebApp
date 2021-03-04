27b7ceea77dd41262369ddbd6c2a54253574caa4
HenryBlairG <henry.blair.g@gmail.com>
2021-03-04 16:09:49 -0300

    :card_file_box: Added Country Model Validations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,9 @@
 class Country < ApplicationRecord
+  # Associations
   has_many :area, dependent: :destroy
   belongs_to :region
+
+  # Validations
+  validates_associated :area, :region
+  validates :c_ID, :c_local_name, presence: true
 end
