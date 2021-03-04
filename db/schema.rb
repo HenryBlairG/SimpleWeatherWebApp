7fefacd65f85ca53cd3a7940857249c2885e132c
HenryBlairG <henry.blair.g@gmail.com>
2021-03-04 16:55:45 -0300

    :card_file_box: Added Area DB Validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_194355) do
+ActiveRecord::Schema.define(version: 2021_03_04_194944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "areas", force: :cascade do |t|
-    t.string "a_ID"
-    t.string "a_local_name"
+    t.string "a_ID", null: false
+    t.string "a_local_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
