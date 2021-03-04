3b42a5a096522fcc2c6b1a75e25971165237e722
HenryBlairG <henry.blair.g@gmail.com>
2021-03-04 16:43:03 -0300

    :card_file_box: Added Region DB Validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_162519) do
+ActiveRecord::Schema.define(version: 2021_03_04_193208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_162519) do
   end
 
   create_table "regions", force: :cascade do |t|
-    t.string "r_ID"
-    t.string "r_local_name"
+    t.string "r_ID", null: false
+    t.string "r_local_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
