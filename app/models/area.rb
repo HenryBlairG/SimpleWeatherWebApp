27fff783b31e3f85eff0737528f70bcf874e4769
HenryBlairG <henry.blair.g@gmail.com>
2021-03-04 16:05:03 -0300

    :card_file_box: Added Area Model Validations

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,9 @@
 class Area < ApplicationRecord
+  # Associations
   has_many :city, dependent: destroy
   belongs_to :country
+  
+  # Validations
+  validates_associated :area, :country
+  validates :a_ID, :a_local_name, presence: true
 end
