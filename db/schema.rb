0a2becb8eac974b55628426988eacb94a3794dda
HenryBlairG <henry.blair.g@gmail.com>
2021-03-04 16:48:07 -0300

    :card_file_box: Added Country DB Validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_193208) do
+ActiveRecord::Schema.define(version: 2021_03_04_194355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_193208) do
   end
 
   create_table "countries", force: :cascade do |t|
-    t.string "c_ID"
-    t.string "c_local_name"
+    t.string "c_ID", null: false
+    t.string "c_local_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
